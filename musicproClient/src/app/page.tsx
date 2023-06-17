30039a294705005a29fc730b268721298b1bde10
notGabo <ga.soto@duocuc.cl>
2023-06-17 18:21:50 -0400

    modificaciones index

--- musicproClient/src/app/page.tsx
+++ musicproClient/src/app/page.tsx
@@ -1,7 +1,8 @@
+"use client"
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
-function Integrantes(){
+function Integrantes() {
   const listaIntegrantes = [
     {
       nombre: 'Alfredo Galdames',
@@ -17,43 +18,42 @@ function Integrantes(){
     },
     {
       nombre: 'Gabriel Soto',
-      github: 'www.github.com/notGabo'
+      github: 'https://github.com/notGabo'
     }
   ];
 
   const router = useRouter();
 
+  // do a map of every integrante item and redirect to the github page of each one
+  const htmlCode = listaIntegrantes.map((integrante, index) => (
+    <li key={index}>
+      <Link href={integrante.github}>{integrante.nombre}</Link>
+    </li>
+  ));
+  return htmlCode;
 }
 
 export default function Home() {
-  
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <img src="/logo.png" alt="MusicPro Logo" width={200} height={200} />
+      <img src="/img/logo.png" alt="MusicPro Logo" width={200} height={200} />
       <h1 className="text-6xl font-bold mt-10">MusicPro</h1>
       <div className="m-5 text-2xl text-center">
         <p>Trabajo para integración de plataformas DuocUC Sede Maipú 2023</p>
         <p>Profesor evaluador: Francisco Valdivieso</p>
         <p>Integrantes:</p>
         <ul>
-          {integrantes.map((integrante, index) => (
-            <li key={index}>
-              <a href=''>{integrante.nombre}</a>
-            </li>
-          ))}
+          <Integrantes />
         </ul>
       </div>
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-        <Link href="/login" className="p-6 mt-6 text-left border w-96 rounded-xl transition duration-500 hover:text-blue-600 focus:text-blue-600">
-          
+        <Link href="/login" className="p-6 mt-6 text-left border w-96 rounded-xl transition duration-500 hover:shadow-2xl hover:shadow-red-800 hover:text-red-800 focus:text-red-800">
             <h3 className="text-2xl font-bold">Login &rarr;</h3>
             <p className="mt-4 text-xl">
               Inicia sesión para poder acceder a la plataforma
             </p>
-          
         </Link>
-        <Link href="/register" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+        <Link href="/register" className="p-6 mt-6 text-left border w-96 rounded-xl hover:shadow-2xl hover:shadow-red-800 hover:text-amber-800 focus:text-amber-800 ">
           
             <h3 className="text-2xl font-bold">Register &rarr;</h3>
             <p className="mt-4 text-xl">

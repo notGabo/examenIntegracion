e3729135575957fdefbe006a6ca85e87d65a1f46
Gabriel Soto <gsoto@analytics.cl>
2023-06-17 17:21:37 -0400

    avance pagina pricipal

--- musicproClient/src/app/page.tsx
+++ musicproClient/src/app/page.tsx
@@ -1,35 +1,67 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+function Integrantes(){
+  const listaIntegrantes = [
+    {
+      nombre: 'Alfredo Galdames',
+      github: 'https://github.com/XLangley'
+    },
+    {
+      nombre: 'Fabian Muñoz',
+      github: 'https://github.com/Sfreimx'
+    },
+    {
+      nombre: 'Fernando Muñoz',
+      github: 'https://github.com/Namnoh'
+    },
+    {
+      nombre: 'Gabriel Soto',
+      github: 'www.github.com/notGabo'
+    }
+  ];
+
+  const router = useRouter();
+
+}
 
 export default function Home() {
-  return (
-    // do a login and register form 
+  
 
-    // instead of use <a> tag, use <Link> tag from next/link
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Image src="/logo.png" alt="MusicPro Logo" width={200} height={200} />
+      <img src="/logo.png" alt="MusicPro Logo" width={200} height={200} />
       <h1 className="text-6xl font-bold mt-10">MusicPro</h1>
-      <p className="mt-3 text-2xl">Trabajo para integracion de plataformas DuocUC Sede Maipu 2023 - Profesor evaluador Francisco Valdivieso</p>
+      <div className="m-5 text-2xl text-center">
+        <p>Trabajo para integración de plataformas DuocUC Sede Maipú 2023</p>
+        <p>Profesor evaluador: Francisco Valdivieso</p>
+        <p>Integrantes:</p>
+        <ul>
+          {integrantes.map((integrante, index) => (
+            <li key={index}>
+              <a href=''>{integrante.nombre}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-        <Link href="/login" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+        <Link href="/login" className="p-6 mt-6 text-left border w-96 rounded-xl transition duration-500 hover:text-blue-600 focus:text-blue-600">
           
             <h3 className="text-2xl font-bold">Login &rarr;</h3>
             <p className="mt-4 text-xl">
-              Inicia sesion para poder acceder a la plataforma
+              Inicia sesión para poder acceder a la plataforma
             </p>
+          
         </Link>
-        
         <Link href="/register" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+          
             <h3 className="text-2xl font-bold">Register &rarr;</h3>
             <p className="mt-4 text-xl">
-              Registrate para poder acceder a la plataforma
+              Regístrate para poder acceder a la plataforma
             </p>
+          
         </Link>
       </div>
     </div>
-
-
-
- 
-  )
+  );
 }

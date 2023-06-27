05b49ac4447cef2b049159806bc667ff208eb853
Gabriel Soto <gsoto@analytics.cl>
2023-06-27 07:07:10 -0400

    tryfix3 api login

--- musicproClient/src/app/api/auth/logout/route.ts
+++ musicproClient/src/app/api/auth/logout/route.ts
@@ -5,28 +5,19 @@ import { useRouter } from 'next/navigation'
 
 export async function GET(request: NextRequest, response: NextResponse) {
 
-    // get cookie
-    const cookie = request.cookies.get('myToken')
-    if (cookie) {
-      const token = jwt.sign({
-        exp: -1,
-        email: '',
-        password: '',
-        username: '',
-      }, 'secret')
-      const serialized = serialize('myToken', token, {
-        httpOnly: true,
-        secure: false,
-        maxAge: -1,
-        path: '/',
-      })
-      
-  
-      response.headers.append('Set-Cookie', serialized)
-      return new Response(JSON.stringify({mensaje: 'Sesion cerrada'}), {status:200, statusText: 'Sesion cerrada'})
+    try{
+      request.cookies.delete('myToken')
+      return new Response(JSON.stringify({
+        message: "Sesion cerrada",
+        data: null
+      }), { status: 200 });
     }
-    return new Response(JSON.stringify({mensaje: 'No se ha iniciado sesion'}), {status:201, statusText: 'No se ha iniciado sesion'})
-
+    catch (error) {
+        return new Response(JSON.stringify({
+            message: "Error al obtener el perfil",
+            error: error
+        }), { status: 500 });
+    }
   }
   
 
